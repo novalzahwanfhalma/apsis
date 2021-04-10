7e39fd6a885f33315bd793457894737d6900caaf
Soeng Souy <soengsouy@gmail.com>
2021-04-11 06:02:22 +0700

    add reset and forgot password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,116 +1,60 @@
 
 {{-- @include('layouts.app') --}}
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <meta name="description" content=""/>
-    <meta name="author" content=""/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>soengsouy.com</title>
-    <!-- loader-->
-    <link href="assets/css/pace.min.css" rel="stylesheet"/>
-    <script src="assets/js/pace.min.js"></script>
-    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
-    <!-- Bootstrap core CSS-->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
-    <!-- animate CSS-->
-    <link href="assets/css/animate.css" rel="stylesheet" type="text/css"/>
-    <!-- Icons CSS-->
-    <link href="assets/css/icons.css" rel="stylesheet" type="text/css"/>
-    <!-- Custom Style-->
-    <link href="assets/css/app-style.css" rel="stylesheet"/>
-    
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="assets/css/pages/auth.css">
 </head>
-    
-    <body class="bg-theme bg-theme1">
-        <!-- Start wrapper-->
-        <div id="wrapper">
-            <div class="height-100v d-flex align-items-center justify-content-center">
-                <div class="card card-authentication1 mb-0">
-                    <div class="card-body">
-                        <div class="card-content p-2">
-                            <div class="card-title text-uppercase pb-2 text-center">Reset Password</div>
-                            @if (session('message'))
-                                <div class="text-success text-center" role="alert">
-                                    {{ session('message') }}
-                                </div>
-                            @endif
-                            <form class="md-float-material" method="POST" action="/forget-password">
-                            @csrf
-                            <div class="form-group">
-                                <label for="exampleInputEmailAddress" class="">Email Address</label>
-                                <div class="position-relative has-icon-right">
-                                    <input type="email" id="exampleInputEmailAddress" class="form-control input-shadow  @error('email') is-invalid @enderror" name="email" value="{{ old('email') }} " placeholder="Email Address">
-                                    <div class="form-control-position">
-                                        <i class="icon-envelope-open"></i>
-                                    </div>
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+<body>
+    <div id="auth">
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo"></a>
+                    </div>
+                    <h1 class="auth-title">Forgot Password</h1>
+                    <p class="auth-subtitle mb-5">Input your email and we will send you reset password link.</p>
+                    @if (session('message'))
+                        <div class="text-success text-center" role="alert">
+                            {{ session('message') }}
+                        </div>
+                    @endif
+                    <br>
+                    <form class="md-float-material" method="POST" action="/forget-password">
+                        @csrf
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="email" class="form-control form-control-xl @error('email') is-invalid @enderror" name="email" value="{{ old('email') }} " placeholder="Email Address">
+                            <div class="form-control-icon">
+                                <i class="bi bi-envelope"></i>
                             </div>
-
-                            <button type="submit" class="btn btn-light btn-block mt-3">SEND RESET LINK</button>
-                        </form>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Send</button>
+                    </form>
+                    <div class="text-center mt-5 text-lg fs-4">
+                        <p class='text-gray-600'>Remember your account? <a href="{{ route('login') }}" class="font-bold">Login</a>.</p>
                     </div>
                 </div>
-                <div class="card-footer text-center py-3">
-                    <p class="text-warning mb-0">Return to the <a href="{{ route('login') }}"> Sign In</a></p>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
                 </div>
             </div>
         </div>
-            
-        <!--Start Back To Top Button-->
-        <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
-        <!--End Back To Top Button-->
-            
-        <!--start color switcher-->
-        <div class="right-sidebar">
-            <div class="switcher-icon">
-                <i class="zmdi zmdi-settings zmdi-hc-spin"></i>
-            </div>
-            <div class="right-sidebar-content">
-                <p class="mb-0">Gaussion Texture</p>
-                <hr>
-                <ul class="switcher">
-                    <li id="theme1"></li>
-                    <li id="theme2"></li>
-                    <li id="theme3"></li>
-                    <li id="theme4"></li>
-                    <li id="theme5"></li>
-                    <li id="theme6"></li>
-                </ul>
-                <p class="mb-0">Gradient Background</p>
-                <hr>
-                <ul class="switcher">
-                    <li id="theme7"></li>
-                    <li id="theme8"></li>
-                    <li id="theme9"></li>
-                    <li id="theme10"></li>
-                    <li id="theme11"></li>
-                    <li id="theme12"></li>
-                    <li id="theme13"></li>
-                    <li id="theme14"></li>
-                    <li id="theme15"></li>
-                </ul>
-            </div>
-        </div>
-        <!--end color switcher-->
-    </div><!--wrapper-->
-    <!-- Bootstrap core JavaScript-->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <!-- sidebar-menu js -->
-    <script src="assets/js/sidebar-menu.js"></script>
-    <!-- Custom scripts -->
-    <script src="assets/js/app-script.js"></script>
+    </div>
+
 </body>
 </html>
-    
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,163 +1,76 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <meta name="description" content=""/>
-    <meta name="author" content=""/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>soengsouy.com</title>
-    <!-- loader-->
-    <link href="{{ URL::to('assets/css/pace.min.css') }}" rel="stylesheet"/>
-    <script src="assets/js/pace.min.js"></script>
-    <!--favicon-->
-    <link rel="icon" href="{{ URL::to('assets/images/favicon.ico') }}" type="image/x-icon">
-    <!-- Bootstrap core CSS-->
-    <link href="{{ URL::to('assets/css/bootstrap.min.css') }}" rel="stylesheet"/>
-    <!-- animate CSS-->
-    <link href="{{ URL::to('assets/css/animate.css') }}" rel="stylesheet" type="text/css"/>
-    <!-- Icons CSS-->
-    <link href="{{ URL::to('assets/css/icons.css') }}" rel="stylesheet" type="text/css"/>
-    <!-- Custom Style-->
-    <link href="{{ URL::to('assets/css/app-style.css') }}" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('assets/css/pages/auth.css') }}">
 </head>
 
-<body class="bg-theme bg-theme1">
-       <!-- start loader -->
-       <div id="pageloader-overlay" class="visible incoming"><div class="loader-wrapper-outer"><div class="loader-wrapper-inner" ><div class="loader"></div></div></div></div>
-       <!-- end loader -->
-           <!-- Start wrapper-->
-           <div id="wrapper">
-               <div class="card card-authentication1 mx-auto my-4">
-                   <div class="card-body">
-                       <div class="card-content p-2">
-                           <div class="text-center">
-                               <img src="{{ URL::to('assets/images/logo-icon.png') }}" alt="logo icon">
-                           </div>
-                           <div class="card-title text-uppercase text-center py-3">Reset Passwor</div>
-                           <form method="POST" action="/reset-password" class="md-float-material">
-                               @csrf
-                               <input type="hidden" name="token" value="{{ $token }}">
-                               <div class="form-group">
-                                   <label for="exampleInputEmailId" class="sr-only">Email</label>
-                                   <div class="position-relative has-icon-right">
-                                       <input type="text" id="exampleInputEmailId" class="form-control input-shadow @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Enter Your Email">
-                                       <div class="form-control-position">
-                                           <i class="icon-envelope-open"></i>
-                                       </div>
-                                       @error('email')
-                                           <span class="invalid-feedback" role="alert">
-                                               <strong>{{ $message }}</strong>
-                                           </span>
-                                       @enderror
-                                   </div>
-                               </div>
-                   
-                               <div class="form-group">
-                                   <label for="exampleInputPassword" class="sr-only">Password</label>
-                                   <div class="position-relative has-icon-right">
-                                       <input type="password" id="exampleInputPassword" class="form-control input-shadow @error('password') is-invalid @enderror" name="password" placeholder="Choose Password">
-                                       <div class="form-control-position">
-                                           <i class="icon-lock"></i>
-                                       </div>
-                                       @error('password')
-                                           <span class="invalid-feedback" role="alert">
-                                               <strong>{{ $message }}</strong>
-                                           </span>
-                                       @enderror
-                                   </div>
-                               </div>
-   
-                               <div class="form-group">
-                                   <label for="exampleInputPassword" class="sr-only">Confirm Password</label>
-                                   <div class="position-relative has-icon-right">
-                                       <input type="password" id="exampleInputPassword" class="form-control input-shadow" name="password_confirmation" placeholder="Choose Confirm Password">
-                                       <div class="form-control-position">
-                                           <i class="icon-lock"></i>
-                                       </div>
-                                   </div>
-                               </div>
-   
-                               <div class="form-group">
-                                   <div class="icheck-material-white">
-                                       <input type="checkbox" id="user-checkbox" checked="" />
-                                       <label for="user-checkbox">I Agree With Terms & Conditions</label>
-                                   </div>
-                               </div>
-                   
-                               <button type="submit" class="btn btn-light btn-block waves-effect waves-light">Reset Password</button>
-                               <div class="text-center mt-3">Sign Up With</div>
-                       
-                               <div class="form-row mt-4">
-                                   <div class="form-group mb-0 col-6">
-                                       <button type="button" class="btn btn-light btn-block"><i class="fa fa-facebook-square"></i> Facebook</button>
-                                   </div>
-                                   <div class="form-group mb-0 col-6 text-right">
-                                       <button type="button" class="btn btn-light btn-block"><i class="fa fa-twitter-square"></i> Twitter</button>
-                                   </div>
-                               </div>
-                           </form>
-                       </div>
-                   </div>
-                   <div class="card-footer text-center py-3">
-                       <p class="text-warning mb-0">Already have an account? <a href="{{ route('login') }}"> Sign In here</a></p>
-                   </div>
-               </div>
-           
-               <!--Start Back To Top Button-->
-               <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
-               <!--End Back To Top Button-->
-           
-               <!--start color switcher-->
-               <div class="right-sidebar">
-                   <div class="switcher-icon">
-                       <i class="zmdi zmdi-settings zmdi-hc-spin"></i>
-                   </div>
-                   <div class="right-sidebar-content">
-   
-                   <p class="mb-0">Gaussion Texture</p>
-                   <hr>
-                   
-                   <ul class="switcher">
-                       <li id="theme1"></li>
-                       <li id="theme2"></li>
-                       <li id="theme3"></li>
-                       <li id="theme4"></li>
-                       <li id="theme5"></li>
-                       <li id="theme6"></li>
-                   </ul>
-   
-                   <p class="mb-0">Gradient Background</p>
-                   <hr>
-                   
-                   <ul class="switcher">
-                       <li id="theme7"></li>
-                       <li id="theme8"></li>
-                       <li id="theme9"></li>
-                       <li id="theme10"></li>
-                       <li id="theme11"></li>
-                       <li id="theme12"></li>
-                       <li id="theme13"></li>
-                       <li id="theme14"></li>
-                       <li id="theme15"></li>
-                   </ul>
-           
-               </div>
-           </div>
-           <!--end color switcher-->
-       
-       </div><!--wrapper-->
-    
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ URL::to('assets/js/jquery.min.js') }}"></script>
-    <script src="{{ URL::to('assets/js/popper.min.js') }}"></script>
-    <script src="{{ URL::to('assets/js/bootstrap.min.js') }}"></script>
-    <!-- sidebar-menu js -->
-    <script src="{{ URL::to('assets/js/sidebar-menu.js') }}"></script>
-    <!-- Custom scripts -->
-    <script src="{{ URL::to('assets/js/app-script.js') }}"></script>
-    
-</body>
-</html>
+<body>
+    <div id="auth">
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="index.html"><img src="{{ URL::to('assets/images/logo/logo.png') }}" alt="Logo"></a>
+                    </div>
+                    <h1 class="auth-title">Reset Password</h1>
+                    <p class="auth-subtitle mb-5">Input your email to register reset new password.</p>
+                    <form method="POST" action="/reset-password" class="md-float-material">
+                        @csrf
+                        <input type="hidden" name="token" value="{{ $token }}">
+                        
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-xl @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Enter Your Email">
+                            <div class="form-control-icon">
+                                <i class="bi bi-envelope"></i>
+                            </div>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl @error('password') is-invalid @enderror" name="password" placeholder="Choose Password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
 
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" name="password_confirmation" placeholder="Choose Confirm Password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Reset Password</button>
+                    </form>
+                    <div class="text-center mt-5 text-lg fs-4">
+                        <p class='text-gray-600'>Already have an account? <a href="{{ route('login') }}"
+                        class="font-bold">Login</a>.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
